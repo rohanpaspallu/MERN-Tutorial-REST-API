56750126fb837b3867ec13482757cd4a96515cb7
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2023-12-06 13:40:04 -0500

    created initial working model for react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World </h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
