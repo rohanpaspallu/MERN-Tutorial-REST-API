3b967b3659bf34b7952b22696a001cbdf4a1d799
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2023-12-11 07:15:31 -0500

    added some changes to work upon goals slice

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import GoalForm from '../components/GoalForm'
+import { getGoals, reset } from '../features/goals/goalSlice'
+import Spinner from '../components/Spinner'
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    const { user } = useSelector(state => state.auth)
+    const { goals, isLoading, isError, message } = useSelector(state => state.goal)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    console.log("value of user is  :", user)
+    useEffect(() => {
+        if (isError)
+            console.log("error message is  : ", message)
+        console.log("comes here")
+        if (!user)
+            navigate('/login')
+
+        dispatch(getGoals())
+        return () => {
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch])
+    return (
+        <>
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+
+            </section>
+            <GoalForm />
+        </>
+    )
 }
 
 export default Dashboard
